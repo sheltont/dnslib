5567adc9117c9538cd59adc4dc38b18922a3d7ab
sheltont <shelton.ms@gmail.com>
2016-06-02 17:47:57 +0800

    fix the listening local address

--- start_outbound_router.sh
+++ start_outbound_router.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup python3 -m dnslib.geointerceptresolver --address 172.21.175.243 --port 53 --log-prefix --upstream 8.8.8.8:53 >./log/inbound.log 2>./log/inbound.log &
+nohup python3 -m dnslib.geointerceptresolver --address 172.21.175.241 --port 53 --log-prefix --upstream 8.8.8.8:53 >./log/inbound.log 2>./log/inbound.log &
