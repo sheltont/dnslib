68bf1a3a230d15ffbd48c3aadf019cd9eaea0548
sheltont <shelton.ms@gmail.com>
2016-06-02 18:01:05 +0800

    fix the listening local address

--- start_inbound_router.sh
+++ start_inbound_router.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup python3 -m dnslib.geozoneresolver --address 172.21.175.243 --port 53 --log-prefix --zonedir ./data/zone/oversea.ceair.com >./log/inbound.log 2>./log/inbound.log &
+nohup python3 -m dnslib.geozoneresolver --address 172.21.175.243 --port 53 --tcp --log-prefix --zonedir ./data/zone/oversea.ceair.com >./log/inbound.log 2>./log/inbound.log &
